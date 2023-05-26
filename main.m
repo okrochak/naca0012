1a7544e700c99d8f2f60a60c4c052de5c7a00cd4
Oleksandr Krochak <O.O.Krochak@student.tudelft.nl>
2023-05-26 17:55:38 +0200

    clean and implement piv

--- main.m
+++ main.m
@@ -5,10 +5,10 @@ close all
 clc
 %% 0. Inputs
 % Folder names with HWA data
-hwa_calib = 'hotwire/calibration/Calibration_%03d';
-hwa_data = 'hotwire/data/';
-piv_data = 'piv/data/'; % directory of piv data
-piv_img = 'piv/images/'; % directory of piv images
+hwa_calib = '../hotwire/calibration/Calibration_%03d';
+hwa_data = '../hotwire/data/';
+piv_data = '../piv/data/'; % directory of piv data
+piv_img = '../piv/images/'; % directory of piv images
 fntSz = 15;
 lblSz = 15;
 Ny = 21; % number of y-measurements
@@ -20,7 +20,7 @@ kings = calibration(hwa_calib); % run the calibration routine to get kings coeff
 % Process 1 file to get the size
 
 % Get the high-frequency measurement of the freestream
-[ts,vs] = processHWA('hotwire/CorrelationTest');
+[ts,vs] = processHWA('../hotwire/CorrelationTest');
 us = polyval(kings, vs);
 mean1 = mean(us);
 std1 = std(us);
@@ -123,13 +123,79 @@ uk_pre = uk .* frequencies';
 spectrum = abs(uk_pre);
 
 
+
+close all
 %% 3. Analyze the PIV data 
-piv_inst_files = {'AoA_0__MP(3x32x32_50ov)./B00001.dat'};
+piv_inst_files = {'AoA_0__SP(32x32_50ov)./B00001.dat', 'AoA_5__SP(32x32_50ov)./B00001.dat', 'AoA_15__SP(32x32_50ov)./B00001.dat'};
+piv_figure = {'AoA_0_32x32_50ov','AoA_5_32x32_50ov','AoA_15_32x32_50ov'};
 for i = 1:length(piv_inst_files)
     dat_file = piv_inst_files{i};
     mat = importdata(strcat(piv_data,dat_file));
-end
+    mat = mat.data;
+    Xog = mat(:,1); Yog = mat(:,2);
+    mat(mat(:,5) == 0,1:4) = nan; % remove invalid points
+%     quiver(mat(:,1),mat(:,2),mat(:,3),mat(:,4))
+    X = mat(:,1); Y = mat(:,2); U = mat(:,3); V = mat(:,4);
 
+
+    nanIndices = isnan(X) | isnan(Y) | isnan(U);
+
+    % Define the grid on which to interpolate
+    xi = linspace(min(X), max(X), 100); % X grid points
+    yi = linspace(min(Y), max(Y), 100); % Y grid points
+    [xi, yi] = meshgrid(xi, yi);
+    
+    % Perform the interpolation
+    Ui = griddata(X(~nanIndices), Y(~nanIndices), U(~nanIndices), xi, yi);
+    
+    % Create the contour plot
+%     contourf(xi, yi, Ui, 'LineWidth', 0.5, 'LineColor', 'k');hold on;
+
+    sp = 3; % quiver spacing
+    contLvl = -5:1:13;
+    tickVals = linspace(contLvl(1), contLvl(end), length(contLvl));
+    %% Define custom colormap
+    numColors = length(contLvl);
+    % Get the "Hot" colormap
+    hotMap = hot(numColors);
+    
+    % Get the "Cold" colormap
+    coldMap = 1 - hotMap;
+    
+    % Create a custom colormap by interpolating between "Hot" and "Cold"
+    hotNcold = [hotMap; coldMap];
+    data = Ui; newdata = data; 
+    for tv = 2:length(contLvl)
+        % find all data between the new tick ranges, one block at a time
+        ind = data>contLvl(tv-1) & data<=contLvl(tv);
+        % Change the corresponding values in the copied data to scale to the
+        % block edges
+        newdata(ind) = rescale(data(ind),tickVals(tv-1),tickVals(tv));
+    end
+        
+    set(gcf,'Position',[100 100 1000 600])
+    contourf(xi,yi,newdata,tickVals); hold on
+    xlim([-20, 160]);ylim([-160, 30]);
+    scatter(Xog(nanIndices), Yog(nanIndices), 'Marker', 'square', 'MarkerFaceColor', 'w', 'MarkerEdgeColor', 'none','SizeData', 130);
+    quiver(X(1:sp:end),Y(1:sp:end),U(1:sp:end),V(1:sp:end),'k');
+    xlim([-20, 160]);ylim([-60, 30]);
+    hold off
+    C=hotNcold;
+    tickVals = linspace(contLvl(1), contLvl(end), length(contLvl)+1);
+    colormap(flipud(C))
+    colorbar('Ticks',tickVals,...
+        'TickLabels',[string(contLvl(1:end)) ""],'TickLabelInterpreter','latex');
+    % colorbar('Ticks',tickVals,...
+    %     'TickLabels',flip({'','5(a)', '4(b)' ,'3(c)', '2(d)', '1(e)' ,'0.5(f)' ,'0(g)' ,'-0.5(h)' ,'-1(i)', '-2(j)', '-3(k)'}) ...
+    %     ,'TickLabelInterpreter','latex');
+    
+    title('$U_{\mathrm{x}}$ [m/s]','interpreter','latex','FontSize',lblSz);
+    xlabel('$x$ [mm]','interpreter','latex','FontSize',lblSz);
+    ylabel('$y$ [mm]','interpreter','latex','FontSize',lblSz);
+    set(gca,'FontSize', fntSz,'TickLabelInterpreter','latex');
+    exportgraphics(gcf,["figures/"+piv_figure{i}+".pdf"], 'Resolution', 300)
+
+end
 %% 3. Plot the results for HWA
 
 % Plot the time scale
